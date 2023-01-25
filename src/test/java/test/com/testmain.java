88f61a63646b8a31c858a2ad69c68f473114a47a
shubham8991405 <shubham8991405@gmail.com>
2023-01-25 11:12:30 +0530

    added java demop app

---
+++ src/test/java/test/com/testmain.java
@@ -0,0 +1,14 @@
+package test.com;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class testmain {
+
+    @Test
+    public void testInputIsEven(){
+    	assertTrue(testing.checkIfInputIsAnEvenNumber(22));
+//    	 assertTrue(testing.checkIfInputIsAnEvenNumber(23));
+    }
+}
